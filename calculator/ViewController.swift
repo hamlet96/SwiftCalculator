9c1c2d5f07f10a2359b48dc767b24bf45b852427
hamlet96 <hamlet96@mail.ru>
2017-06-08 21:23:28 +0300

    Lecture 1 Finished. CS193P

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
-    
+    var displayValue: Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
     var firstSymbol = true
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
@@ -28,7 +35,17 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func performOperation(_ sender: UIButton) {
+        firstSymbol=true
+        
         let operation = sender.currentTitle!
+        switch operation {
+        case "√":
+            displayValue = sqrt(displayValue)
+        case "π":
+            displayValue = Double.pi
+        default:
+            break
+        }
         
     }
     
